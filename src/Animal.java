b389f7681b1a6618269b2f0850be9b3157b32b25
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-19 14:19:38 +0100

    verso di animale

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,11 @@
 public class Animal {
-    
+    private String birdNoise;
+
+    public Animal(String birdNoise) {
+        this.birdNoise = birdNoise;
+    }
+
+    public void makeNoise() {
+        System.out.println(this.birdNoise);
+    }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,16 @@
 public class Cat extends Animal {
-    
-    
+
+    public Cat(String catNoise) {
+        super(catNoise);
+    }
 }
+/*private String catNoise;
+
+public Cat(String catNoise) {
+    this.catNoise = catNoise;
+}
+
+public void makeNoise() {
+    System.out.println("The CAT noise is: " + this.catNoise);
+}
+}*/
